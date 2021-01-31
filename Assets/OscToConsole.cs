258d229aa3805054c2fc85c198874804ed2db707
polinski <paul@65daysofstatic.com>
2021-01-31 15:31:48 +0100

    Initial commit

---
+++ Assets/OscToConsole.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscToConsole : MonoBehaviour
+{
+    public void LogOscStringToConsole(string msg)
+    {
+        Debug.Log(msg);
+    }
+}
